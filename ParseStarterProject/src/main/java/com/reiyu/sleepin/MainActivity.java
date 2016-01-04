47746525d0f3fa67b0e14b37dc6694b67d3cab93
rei___yu <satomi.i.sakura@gmail.com>
2016-01-04 20:44:10 +0900

    refactor--indent

--- ParseStarterProject/src/main/java/com/reiyu/sleepin/MainActivity.java
+++ ParseStarterProject/src/main/java/com/reiyu/sleepin/MainActivity.java
@@ -19,61 +19,61 @@ import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    ParseAnalytics.trackAppOpenedInBackground(getIntent());
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ParseAnalytics.trackAppOpenedInBackground(getIntent());
 
-    SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
-    if (!(sp.getBoolean("@string/signed_in", false))) {
-      startActivity(new Intent(MainActivity.this, SignInFragment.class));
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        if (!(sp.getBoolean("@string/signed_in", false))) {
+            startActivity(new Intent(MainActivity.this, SignInFragment.class));
+        }
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        String date = year + "/" + (month + 1) + "/" + day;
+
+        if ((sp.getString("@string/record_updated", null) == null) || (!(sp.getString("@string/record_updated", null).equals(date)))) {
+            Log.e("RECORD_UPDATED", String.valueOf(date) + ":" +
+                    " data not yet recorded");
+            startActivity(new Intent(MainActivity.this, WakeUpFragment.class));
+        }
+        setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.welcome_message);
+        String msg = sp.getString("@string/name", null) + "'s Flower";
+        textView.setText(msg);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.e("Floating Action Button", "clicked");
+                startActivity(new Intent(MainActivity.this, ReflectFragment.class));
+            }
+        });
     }
 
-    Calendar cal = Calendar.getInstance();
-    int year = cal.get(Calendar.YEAR);
-    int month = cal.get(Calendar.MONTH);
-    int day = cal.get(Calendar.DAY_OF_MONTH);
-    String date = year + "/" + (month + 1) + "/" + day;
-
-    if ((sp.getString("@string/record_updated", null) == null)||(!(sp.getString("@string/record_updated", null).equals(date)))) {
-      Log.e("RECORD_UPDATED", String.valueOf(date) + ":" +
-              " data not yet recorded");
-      startActivity(new Intent(MainActivity.this, WakeUpFragment.class));
-    }
-    setContentView(R.layout.activity_main);
-
-    TextView textView = (TextView) findViewById(R.id.welcome_message);
-    String msg = sp.getString("@string/name", null) + "'s Flower";
-    textView.setText(msg);
-
-    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-    fab.setOnClickListener(new View.OnClickListener() {
-      public void onClick(View v) {
-        Log.e("Floating Action Button", "clicked");
-        startActivity(new Intent(MainActivity.this, ReflectFragment.class));
-      }
-    });
-  }
-
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_main, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_sign_out) {
-      return true;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
-    return super.onOptionsItemSelected(item);
-  }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_sign_out) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
